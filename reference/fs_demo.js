4f6652614d2250e7be667e1bd5fda227dc9c2b65
akhil-ansorbit <akhil.gothi@ansorbit.com>
2022-12-08 18:11:37 +0530

    demo : fs_demo

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -8,6 +8,44 @@ fs.mkdir("test1", (err) => {
 });
 // Folder created mathod-2
 fs.mkdir(path.join(__dirname, "test2"), {}, (err) => {
-  if (err) throw err;
   console.log("Folder2 created successfully");
 });
+
+//Write file
+fs.writeFile(
+  path.join(__dirname, "test2", "hello.txt"),
+  "Helloo John",
+  {},
+  (err) => {
+    if (err) throw err;
+    console.log(
+      "Write successfully................................................................"
+    );
+  }
+);
+
+// Append File
+fs.appendFile(
+  path.join(__dirname, "test2", "hello.txt"),
+  "  Helloo Doe",
+  (err) => {
+    if (err) throw err;
+    console.log("Append File successfully");
+  }
+);
+
+// Read file
+fs.readFile(path.join(__dirname, "test2", "hello.txt"), "utf8", (err, data) => {
+  if (err) throw err;
+  console.log("Read Data -", data);
+});
+
+//Renamed file
+fs.rename(
+  path.join(__dirname, "test2", "hello.txt"),
+  path.join(__dirname, "test2", "name.txt"),
+  (err, data) => {
+    if (err) throw err;
+    console.log("Renamed file");
+  }
+);
