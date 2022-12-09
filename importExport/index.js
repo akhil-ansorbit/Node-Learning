42fc7186227fc825186c58ace4b464f59dd62136
akhil-ansorbit <akhil.gothi@ansorbit.com>
2022-12-09 18:21:06 +0530

    export-import

---
+++ importExport/index.js
@@ -0,0 +1,12 @@
+// const add = require("./oper");
+// console.log(add(5, 5));
+
+// const { add, sub, name } = require("./oper");
+// console.log(add1(15, 5));
+// console.log(sub(15, 5));
+// console.log(name);
+
+const operator = require("./oper");
+console.log(operator.add(5, 5));
+console.log(operator.sub(5, 5));
+console.log(operator.name);
---
+++ importExport/oper.js
@@ -0,0 +1,15 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const name = "Akhil";
+// module.exports = add;
+
+// module.exports.add1 = add;
+// module.exports.sub = sub;
+
+module.exports = { add, sub, name };
