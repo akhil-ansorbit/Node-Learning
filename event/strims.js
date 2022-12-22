7595eb8727de9375eb8f7dc3407cee181dfdc664
akhil-ansorbit <akhil.gothi@ansorbit.com>
2022-12-22 11:38:22 +0530

    strims.pipe

--- event/strims.js
+++ event/strims.js
@@ -3,18 +3,28 @@ const server = http.createServer();
 const fs = require("fs");
 
 server.on("request", (req, res) => {
-  const rstream = fs.createReadStream("../reference/test2/name.txt");
+  // 1 Mathod ______________
+  // fs.readFile("../reference/test2/name.txt", (err, data) => {
+  //   if (err) return console.log(err);
+  //   res.end(data.toString());
+  // });
 
-  rstream.on("data", (chunkdata) => {
-    res.write(chunkdata);
-  });
-  rstream.on("end", () => {
-    res.end();
-  });
-  rstream.on("error", (err) => {
-    console.log(err);
-    res.end("File not found");
-  });
+  //2 Mathod ________________
+  // const rstream = fs.createReadStream("../reference/test2/name.txt");
+  // rstream.on("data", (del) => {
+  //   res.write(del);
+  // });
+  // rstream.on("end", () => {
+  //   res.end();
+  // });
+  // rstream.on("error", (err) => {
+  //   console.log(err);
+  //   res.end("File not found");
+  // });
+
+  //3 Mathod
+  const rstream = fs.createReadStream("../reference/test2/name.txt");
+  rstream.pipe(res);
 });
 
 server.listen(5010);
