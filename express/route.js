0e13b74001d42db7bdde0349a95725cf8df7847b
akhil-ansorbit <akhil.gothi@ansorbit.com>
2022-12-28 18:50:52 +0530

    json format

--- express/route.js
+++ express/route.js
@@ -11,7 +11,38 @@ app.get("/about", (req, res) => {
   res.send("<h1>Helloo Wel-come in about page</h1>");
 });
 
+app.get("/", (req, res) => {
+  res.write("<h1>Helloo Wel-come in about page</h1>");
+  res.write("<h1>Helloo Wel-come in again about page</h1>");
+  res.send();
+});
+
+app.get("/api", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      name: "Pavan",
+    },
+    {
+      id: 2,
+      name: "Harshad",
+    },
+  ]);
+});
+
+app.get("/api/json", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "Pavan",
+    },
+    {
+      id: 2,
+      name: "Harshad",
+    },
+  ]);
+});
+
 app.listen(8010, () => {
   console.log("Listen on http://localhost-8010");
 });
-  
